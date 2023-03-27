54f957b97c8e7e0573de82cc6472c283d1636666
Mahesh Dargad <128609177+MrDargad@users.noreply.github.com>
2023-03-27 23:11:58 +0530

    MUFFINS3

--- 1000-1200 1* Beginner Level/1023 Packaging Cupcakes (MUFFINS3)/Code.cpp
+++ 1000-1200 1* Beginner Level/1023 Packaging Cupcakes (MUFFINS3)/Code.cpp
@@ -2,16 +2,15 @@
 using namespace std;
 
 int main() {
-    int t;
-    cin>>t;
-    while(t--)
-    {   
-        int n;
-        cin>>n;
-        cout<<(n/2)+1<<endl;
-        
-    }
-	
+	int T;
+	cin>>T;
+	while(T--){
+	    int N;
+	    cin>>N;
+	    if(N==2)
+	        cout<<"2\n";
+	       else
+	        cout<<N/2+1<<endl;
+	}
 	return 0;
 }
-
