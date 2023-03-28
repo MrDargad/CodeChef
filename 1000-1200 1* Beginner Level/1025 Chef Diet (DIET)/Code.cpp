3cd647da0df3150bf9646a94fb1a5d52afd0cd45
Mahesh Dargad <128609177+MrDargad@users.noreply.github.com>
2023-03-28 23:21:06 +0530

    DIET

--- 1000-1200 1* Beginner Level/1025 Chef Diet (DIET)/Code.cpp
+++ 1000-1200 1* Beginner Level/1025 Chef Diet (DIET)/Code.cpp
@@ -2,33 +2,28 @@
 using namespace std;
 
 int main() {
-	// your code goes here
-	int t;
-	cin>>t;
-	while(t--)
-	{
-	    int days,required,left=0;
-	    cin>>days>>required;
-	    int protein[days];
-	    for(int i=0 ; i<days ; i++)
-	        cin>>protein[i];
+	int T;
+	cin>>T;
+	while(T--){
+	    int N,K;
+	    cin>>N>>K;
 	    
-	    for (int i=0 ; i<days ; i++)
-	    {   
-	        left=protein[i]-required;
-	        if(left<0)
-	          {   
+	    int calo=0;
+	    int A[N];
+	    for(int i=0;i<N;i++)
+	        cin>>A[i];
+	       
+	   int flag=0;
+	   for(int i=0;i<N;i++){
+	       calo+=A[i]-K;
+	       if(calo<0){
 	            cout<<"NO "<<i+1<<endl;
-                break;
-              }
-            else
-	             protein[i+1]+=left;
-          
-	    }
-	    if(left>=0)
+	            flag=1;
+	            break;
+	        }
+	   }
+	   if(flag==0)
 	        cout<<"YES\n";
-	    
 	}
 	return 0;
 }
-
