ba296887d9b8addd3c0757acf8da6499d2ca9019
Mahesh Dargad <128609177+MrDargad@users.noreply.github.com>
2023-03-28 23:22:57 +0530

    AIRLINE

--- 1000-1200 1* Beginner Level/1042 Airline Restrictions (AIRLINE)/code.cpp
+++ 1000-1200 1* Beginner Level/1042 Airline Restrictions (AIRLINE)/code.cpp
@@ -1,38 +1,16 @@
 #include <iostream>
-#include<algorithm>
 using namespace std;
 
-
 int main() {
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int a[3],total,handReq;
-        for(int i=0 ; i<3 ; i++)
-            cin>>a[i];
-        cin>>total>>handReq;
-        
-        sort(a,a+3);
-        int hand=0,flag=0;        
-        for(int i=0 ; i<3 ; i++)
-        {
-            if(a[i]<=handReq)
-            {
-                hand=a[i];
-                if(a[0]+a[1]+a[2]-hand<=total)
-                {
-                    cout<<"YES\n";
-                    flag=1;
-                    break;
-                }
-                
-            }
-        }
-        if(flag==0)
-            cout<<"NO\n";
-    }
-	
+	int T;
+	cin>>T;
+	while(T--){
+	    int A,B,C,D,E;
+	    cin>>A>>B>>C>>D>>E;
+	    if((A+B<=D && C<=E) || (A+C<=D && B<=E) || (C+B<=D && A<=E))
+	        cout<<"YES\n";
+	    else
+	        cout<<"NO\n";
+	}
 	return 0;
 }
-
