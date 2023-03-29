ea12976e96f52d21bdf3c4b0fc1319432dfd3dd2
Mahesh Dargad <128609177+MrDargad@users.noreply.github.com>
2023-03-29 23:21:42 +0530

    LAPTOPREC

--- 1000-1200 1* Beginner Level/1104 Laptop Recommendation (LAPTOPREC)/code.cpp
+++ 1000-1200 1* Beginner Level/1104 Laptop Recommendation (LAPTOPREC)/code.cpp
@@ -1,25 +1,40 @@
-#include <iostream>
+LAP#include <iostream>
 using namespace std;
 
 int main() {
-  int t;
-  cin>>t;
-  while(t--)
-  {
-      int n;
-      cin>>n;
-      int arr[11]={},max=0,flag=0,no,lap=0;
-      for(int i=0 ; i<n ; i++)
-      {
-        cin>>no;
-        if(max < ++arr[no])
-            max = arr[no], lap = no, flag = 1;
-        else if(max==arr[no])
-            flag=2;
-      }
-      
-        if(flag==1)     cout<<lap<<endl;
-        else    cout<<"CONFUSED\n";
-  }
-    return 0;
+	int T;
+	cin>>T;
+	while(T--){
+        int N;
+        cin>>N;
+        int A[11]={0};
+        
+        int temp;
+        int max=0;
+        for(int i=0;i<N;i++)
+        {
+            cin>>temp;
+            A[l]++;
+            if(A[temp]>max)
+                max=A[l];
+        }
+        int flag=0;
+        int ans;
+        for(int i=0;i<11;i++)
+        {
+            if(A[i]==max)
+            {
+                flag++;
+                ans=i;
+            }
+            if(flag==2)
+            {
+                cout<<"CONFUSED\n";
+                break;
+            }
+        }
+        if(flag!=2)
+            cout<<ans<<endl;
+	}
+	return 0;
 }
