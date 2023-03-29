b3946987a16c29ac0176d9b9a8d7e1143304e6a0
Mahesh Dargad <128609177+MrDargad@users.noreply.github.com>
2023-03-29 23:27:25 +0530

    COVID_19

--- 1000-1200 1* Beginner Level/1077 Covid and Theatre Tickets (COVID_19)/code.cpp
+++ 1000-1200 1* Beginner Level/1077 Covid and Theatre Tickets (COVID_19)/code.cpp
@@ -2,28 +2,16 @@
 using namespace std;
 
 int main() {
-	// your code goes here
-	int t;
-	cin>>t;
-	while(t--)
-	{
-	    int row,seats;
-	    cin>>row>>seats;
-	    //for 1 row
-	    int sr=0;
-	    if(seats%2==0)
-	        sr=seats/2;
-	    else
-	        sr=seats/2+1;
-	    
-	    //for n rows
-	    if(row%2==0)
-	        cout<<(row/2)*sr<<endl;
-	    else
-            cout<<((row/2)+1)*sr<<endl;
-	    
+	int T;
+	cin>>T;
+	while(T--){
+        int N,M;
+        cin>>N>>M;
+        if(N%2==1)
+            N++;
+        if(M%2==1)
+            M++;
+        cout<<(N/2)*(M/2)<<endl;
 	}
-	
 	return 0;
 }
-
