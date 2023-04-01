77701a2675813eef85700792fbcbb3ec0b74a747
Murtuza-kapasi <100362384+MurtuzaKapasi@users.noreply.github.com>
2023-04-02 00:39:33 +0530

    PERMEXIS

---
+++ 1200-1400 1* Advanced Level/1207 Watson asks Does Permutation Exist (PERMEXIS)/code.cpp
@@ -0,0 +1,34 @@
+#include<cmath>
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int  arr[n] ;
+        for(int i=0 ; i<n ; i++)
+            cin>>arr[i];
+            
+        sort(arr,arr+n);
+        int flag=0;
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            if(abs(arr[i]-arr[i+1])>1)
+            {
+                flag=1;
+                cout<<"NO\n";
+                break;
+            }
+        }
+        if(flag==0)
+            cout<<"YES\n";
+    }
+	
+	return 0;
+}
+
