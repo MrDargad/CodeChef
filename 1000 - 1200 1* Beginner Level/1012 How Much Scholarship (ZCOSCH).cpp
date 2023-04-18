75f2a0e8ca021121ff28c136907d252d0f809593
Mahesh Dargad <128609177+MrDargad@users.noreply.github.com>
2023-04-18 23:16:50 +0530

    ZCOSCH

---
+++ 1000 - 1200 1* Beginner Level/1012 How Much Scholarship (ZCOSCH).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N;
+	cin>>N;
+	if(N<=50)
+	    cout<<"100";
+	   else if(N<=100)
+	    cout<<"50"; 
+	   else
+	    cout<<"0";
+	return 0;
+}
