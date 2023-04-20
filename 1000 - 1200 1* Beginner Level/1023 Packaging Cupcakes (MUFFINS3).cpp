1b131f753fc4758b988cf0ce1feac5511f21ac9e
Mahesh Dargad <128609177+MrDargad@users.noreply.github.com>
2023-04-20 23:28:43 +0530

    MUFFINS3

---
+++ 1000 - 1200 1* Beginner Level/1023 Packaging Cupcakes (MUFFINS3).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--){
+	    int N;
+	    cin>>N;
+	    if(N==2)
+	        cout<<"2\n";
+	    else
+	        cout<<N/2+1<<endl;
+	}
+	return 0;
+}
