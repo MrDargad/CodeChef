a8ca342fe42a9e2c6cb7edf4c01d571df6c4fde9
Mahesh Dargad <128609177+MrDargad@users.noreply.github.com>
2023-05-03 18:20:30 +0530

    STUDVOTE

---
+++ 1200 - 1400 1* Advanced Level/1205 Drumpf for President (STUDVOTE).cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--){
+	    int N,K;
+	    cin>>N>>K;
+	    int student;
+	    
+	    int A[N];
+		int A1[N]={0};
+	    for(int i=0;i<N;i++)
+	    {
+	        cin>>student;
+	        A[i]=student-1;
+	        if(A[i]==i)
+	        {
+	            A1[i]=-1;
+	        }
+            else
+	        {
+	            if(A1[A[i]]!=-1)
+	                A1[A[i]]++;
+	        }
+	    }
+	        
+	    int count=0;
+	    
+	    for(int i=0;i<N;i++)
+	    {
+	        if(A1[i]>=K)
+	            count++;
+	    }
+	    cout<<count<<endl;
+	    
+	}
+	return 0;
+}
