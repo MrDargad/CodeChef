e2f661be98ac34a88e3a807414b0aae9050f0a2e
Mahesh Dargad <128609177+MrDargad@users.noreply.github.com>
2023-06-06 22:31:11 +0530

    AIRLINE

---
+++ 1000 - 1200 1* Beginner Level/1042 Airline Restrictions (AIRLINE).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    int A,B,C,D,E;
+	    cin>>A>>B>>C>>D>>E;
+	    if((A+B<=D && C<=E) || (A+C<=D && B<=E) || (C+B<=D && A<=E))
+	        cout<<"YES\n";
+	    else
+	        cout<<"NO\n";
+	}
+	return 0;
+}
