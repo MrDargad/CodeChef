49fd9383070f69ad2d088031e173299510a29360
Mahesh Dargad <128609177+MrDargad@users.noreply.github.com>
2023-06-17 20:34:55 +0530

    EASYPERM

---
+++ 1000 - 1200 1* Beginner Level/1057 Easy Permutation (EASYPERM).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--){
+        int N;
+        cin>>N;
+        while(N>0)
+        {
+            cout<<N<<" ";
+            N--;
+        }
+	    cout<<endl;
+	}
+	return 0;
+}
